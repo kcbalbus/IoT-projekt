cd91e40bcc8455e20a7d40397be3d92f13c95483
Karolina Łukasik <100850964+karolukasik765@users.noreply.github.com>
2024-01-19 18:27:40 +0100

    Update Card.py

--- Card.py
+++ Card.py
@@ -4,7 +4,7 @@ from config import * # pylint: disable=unused-wildcard-import
 from mfrc522 import MFRC522
 from datetime import datetime
 
-def buzzer():
+def buzz():
     GPIO.output(buzzerPin, False)
     time.sleep(1)
     GPIO.output(buzzerPin, True)
@@ -17,28 +17,29 @@ def blink():
 def rfidRead():
     MIFAREReader = MFRC522()
     while True:
-        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        (status, _) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
         if status == MIFAREReader.MI_OK:
-            dt = datetime.now()
-            num = 0
-            for i in range(0, len(uid)):
-                num += uid[i] << (i*8)
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
+            if status == MIFAREReader.MI_OK:
+                dt = datetime.now()
+                num = 0
+                for i in range(0, len(uid)):
+                    num += uid[i] << (i*8)
                 print(f"Card read UID: {num}")
-            buzzer()
-            blink()
-            print(f"Date and time of scanning: {dt}")
-            while status == MIFAREReader.MI_OK:
-                MIFAREReader = MFRC522()
-                MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+                buzz()
+                blink()
+                print(f"Date and time of scan: {dt}")
+                while status == MIFAREReader.MI_OK:
+                    MIFAREReader = MFRC522()
+                    (status, _) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
 
 
 def test():
 
-    print('Place the card close to the reader (on the right side of the set).')
+    print('Place the card close to the reader.')
     rfidRead()
 
 
 if __name__ == "__main__":
     test()
-    GPIO.cleanup() # pylint: disable=no-member
