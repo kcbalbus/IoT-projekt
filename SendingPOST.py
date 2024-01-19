fc0f04b6683f4c16214442e8e625cc9a1cddbf18
Oskar <137009832+OskarGorgis@users.noreply.github.com>
2024-01-19 19:37:50 +0100

    Update SendingPOST.py

--- SendingPOST.py
+++ SendingPOST.py
@@ -1,31 +1,55 @@
 import requests
-import json
+from LoadingMeteorologicalValues import readSensors
 
-# Adres API, do którego będziemy wysyłać zapytanie POST
-api_url = "https://example.com/api/endpoint"
 
-# Dane, które chcemy przesłać (w formie słownika)
-data_to_send = {
-    "key1": "value1",
-    "key2": "value2",
-}
+def sendTo(api_url, raw_data, isCard = False):
 
-# Konwertuj dane na format JSON
-json_data = json.dumps(data_to_send)
+    if isCard:
+        content_type = "CardData"
+    else:
+        content_type = "WeatherData"
+        
+    headers = {
+        "Content-Type": content_type,
+        "Authorization": "",  
+    }
+    
+    # Wysłanie zapytania POST
+    response = requests.post(api_url, data=raw_data)
+    
+    # Sprawdź status odpowiedzi
+    if response.status_code == 200:
+        print("Zapytanie POST zostało pomyślnie wysłane.")
+        print("Odpowiedź serwera:", response.text)
+    else:
+        print("Wystąpił błąd. Kod statusu:", response.status_code)
+        print("Treść odpowiedzi:", response.text)
 
-# Nagłówki (opcjonalne, zależy od wymagań API)
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer YOUR_ACCESS_TOKEN",  # Dodaj token autoryzacyjny, jeśli wymagany
-}
 
-# Wysłanie zapytania POST
-response = requests.post(api_url, data=json_data, headers=headers)
+def sendWeatherData(api_url = "http://10.108.33.110:8000/api/send_weather_data/"):
+    sendTo(api_url, getWeatherData())
 
-# Sprawdź status odpowiedzi
-if response.status_code == 200:
-    print("Zapytanie POST zostało pomyślnie wysłane.")
-    print("Odpowiedź serwera:", response.text)
-else:
-    print("Wystąpił błąd. Kod statusu:", response.status_code)
-    print("Treść odpowiedzi:", response.text)
+
+def sendCardData(card_id, api_url = "http://10.108.33.110:8000/api/send_weather_data/"):
+    sendTo(api_url, card_id, True)
+
+
+def getWeatherData():
+
+    (temp, hum, press) = readSensors()
+
+    print(temp)
+    print(hum)
+    print(press)
+
+
+    data_to_send = {
+        "weather_station": "f4085dc7-9284-4e13-b53f-a6284979ce64",
+        "temperature": temp,
+        "humidity": hum,
+        "pressure": press
+    }
+    return data_to_send
+
+if __name__ == "__main__":
+    sendWeatherData()
