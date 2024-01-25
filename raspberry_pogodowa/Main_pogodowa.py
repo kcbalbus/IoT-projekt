cc48b3af5dd8e15285d86f4c5b64d0adacd7f756
Oskar <137009832+OskarGorgis@users.noreply.github.com>
2024-01-25 19:05:04 +0100

    Create Main_pogodowa.py

---
+++ raspberry_pogodowa/Main_pogodowa.py
@@ -0,0 +1,56 @@
+from queue import Empty
+import time
+import RPi.GPIO as GPIO
+from config import * # pylint: disable=unused-wildcard-import
+from mfrc522 import MFRC522
+from datetime import datetime
+from Card import buzz
+from SendingPOST import notifyServerTime, validateCard, sendWeatherData
+
+
+card_set = set()
+last_send = time.time()
+
+
+def ScanCard():
+    MIFAREReader = MFRC522()
+    while True:
+        sendDataOnce()
+        (status, _) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        if status == MIFAREReader.MI_OK:
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
+            if status == MIFAREReader.MI_OK:
+                dt = datetime.now()
+                num = 0
+                for i in range(0, len(uid)):
+                    num += uid[i] << (i*8)
+                print(f"Card read UID: {num}")
+                print(f"Date and time of scan: {dt}")
+                buzz()
+                if validateCard(num):
+                    notifyServerTime(num)
+                    notify_set(num)
+                    print(card_set)
+                while status == MIFAREReader.MI_OK:
+                    MIFAREReader = MFRC522()
+                    sendDataOnce()
+                    (status, _) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+
+def notify_set(id):
+    if id in card_set:
+        card_set.remove(id)
+    else:
+        card_set.add(id)
+
+
+def sendDataOnce():
+    global last_send
+    curr_time = time.time()
+    if card_set and (curr_time - last_send > 10):
+        last_send = curr_time
+        sendWeatherData()
+
+
+if __name__ == "__main__":
+    ScanCard()
